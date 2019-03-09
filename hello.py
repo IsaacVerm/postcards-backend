1740ec7c3ba8126f7de1868dd34da5ecc58de984
IsaacVerm <isaacverm@gmail.com>
2019-03-09 16:32:00 +0100

    Add basic example to test and deploy

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 
 @app.route('/<name>')
-def hello(name):
-    return render_template("hello.html", name=name)
+def credentials(name):
+    if name == "me":
+        return jsonify(user='myself', password='sense')
+    if name == "you":
+        return jsonify(user='yourself', password='nonsense')
